cc56dab820d4521363b0f9e0a64052267da0cd8d
Niels Filmer <niels.filmer@boredofadvisors.com>
2016-07-14 15:53:25 +0200

    Replaced font awesome with glyphicons

--- src/views/cells/actions.blade.php
+++ src/views/cells/actions.blade.php
@@ -1,3 +1,3 @@
-@if($btn_show)<a data-text="view" href="/{{ $slug }}/{{ $key }}"><i class="fa fa-search"></i></a> @endif
-@if($btn_edit)<a data-text="edit" href="/{{ $slug }}/{{ $key }}/edit"><i class="fa fa-edit"></i></a> @endif
-@if($btn_destroy)<a class="rest" data-method="DELETE" data-text="delete" href="/{{ $slug }}/{{ $key }}"><i class="fa fa-trash"></i></a>@endif
+@if($btn_show)<a data-text="view" href="/{{ $slug }}/{{ $key }}"><i class="glyphicon glyphicon-search"></i></a> @endif
+@if($btn_edit)<a data-text="edit" href="/{{ $slug }}/{{ $key }}/edit"><i class="glyphicon glyphicon-edit"></i></a> @endif
+@if($btn_destroy)<a class="rest" data-method="DELETE" data-text="delete" href="/{{ $slug }}/{{ $key }}"><i class="glyphicon glyphicon-trash"></i></a>@endif
