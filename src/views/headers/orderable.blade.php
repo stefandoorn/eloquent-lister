16ac69169bd202ed30406a08a1489a50f95b64a3
Niels Filmer <niels.filmer@boredofadvisors.com>
2016-08-08 10:20:31 +0200

    Added bootstrap caret

--- src/views/headers/orderable.blade.php
+++ src/views/headers/orderable.blade.php
@@ -1,1 +1,10 @@
-<a href="{{ $url }}" class="lister-order-link" data-order="{{ $order }}">{{ $display }}</a>
+<a href="{{ $url }}" class="lister-order-link" data-order="{{ $order }}">
+    {{ $display }}
+    @if($order)
+        @if($order == 'asc')
+            <span class="glyphicon glyphicon-triangle-bottom"></span>
+        @else
+            <span class="glyphicon glyphicon-triangle-top"></span>
+        @endif
+    @endif
+</a>
