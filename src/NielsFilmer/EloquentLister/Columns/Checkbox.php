9faf945b4d2a65242b3910804cf07c443bff9c58
Niels Filmer <niels@awfulmen.nl>
2017-01-25 17:56:11 +0100

    Added checkbox type

---
+++ src/NielsFilmer/EloquentLister/Columns/Checkbox.php
@@ -0,0 +1,23 @@
+<?php namespace NielsFilmer\EloquentLister\Columns;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Plain extends BaseColumn {
+
+    protected static $view = 'eloquent-lister::cells.checkbox';
+
+    /**
+     * @param Model $row
+     *
+     * @return \Illuminate\View\View
+     */
+    public function makeCell(Model $row)
+    {
+        $attribute = $this->attribute;
+        $accessor = $this->accessor;
+        $value = $accessor($row->$attribute, $row);
+        return view(static::$view, compact('value'));
+    }
+
+}
